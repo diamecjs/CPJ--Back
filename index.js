fce9f4004a5e802ecb5135f7a735c10f5ffb1567
DiameCjs <diame.cjs@gmail.com>
2023-03-09 12:31:51 -0500

    cambios

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {
 
 const port = process.env.PORT || 3001;
 // Syncing all the models at once
-conn.sync({ force: false}).then(async () => {
+conn.sync({ force: true}).then(async () => {
     loadCategory();
     loadSubCategory();
     server.listen(port, () => {
